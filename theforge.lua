f66112a1b2c8b5ec35a4f61cfaa1836aad00d56b
ryx67 <rtho49401@gmail.com>
2025-12-07 16:01:14 +0700

    updated

--- theforge.lua
+++ theforge.lua
@@ -7,47 +7,55 @@ local debrisService = cloneref(game:GetService('Debris'))
 local lplr = playersService.LocalPlayer
 
 local vars = {
-    tweenspeed = 55
+    tweenspeed = 55,
+    automine = true,
 }
 
 local function tweenTo(Position)
     local Root = lplr.Character.HumanoidRootPart
-    local UpperPos = Vector3.new(Position.X, Position.Y, Position.Z)
+    local Pos = Vector3.new(Position.X, Position.Y, Position.Z)
 
-    local Distance = (Root.Position - UpperPos).Magnitude
+    local Distance = (Root.Position - Pos).Magnitude
     local Tween = tweenService:Create(
         Root,
         TweenInfo.new(Distance / vars.tweenspeed, Enum.EasingStyle.Linear),
-        { CFrame = CFrame.new(UpperPos) }
+        {
+            CFrame = CFrame.new(Pos)
+        }
     )
     Tween:Play()
     Tween.Completed:Wait()
 end
 
 local function Noclip()
-    for i, v in pairs(plr.Character:GetDescendants()) do
-        if v:IsA("BasePart") and v.CanCollide == true then
+    for i, v in pairs(lplr.Character:GetDescendants()) do
+        if v:IsA('Part') and v.CanCollide == true then
             v.CanCollide = false
         end
     end
 end
 
 local function getRocks(Name)
-    for k, i in pairs(workspace.Rocks:GetChildren()) do
-        for z, v in pairs(i:GetChildren()) do
-            if v:IsA('Part') and v:FindFirstChildOfClass('Model') then
-                local Target = v:FindFirstChildOfClass('Model')
-                if Target.Name == Name and Target:GetAttribute('Health') ~= 0 then
-                    return Target
-                end
-            end
-        end
-    end
-    return nil
+	for _, i in pairs(workspace.Rocks:GetChildren()) do
+		for _, v in pairs(i:GetChildren()) do
+			if v:IsA('Part') and v:FindFirstChildOfClass('Model') then
+				local Target = v:FindFirstChildOfClass('Model')
+				if Target.Name == Name and Target:GetAttribute('Health') > 0 then
+					return Target.Hitbox
+				end
+			end
+		end
+	end
 end
 
+
 while task.wait() do
-    tweenTo(getRocks('Pebble').PrimaryPart.Position)
-    replicatedStorage.Shared.Packages.Knit.Services.ToolService.RF.ToolActivated:InvokeServer('Pickaxe')
-    Noclip()
+    local Slave = getRocks('Pebble')
+    if Slave then
+        tweenTo(getRocks('Pebble').Position)
+        replicatedStorage.Shared.Packages.Knit.Services.ToolService.RF.ToolActivated:InvokeServer('Pickaxe')
+        Noclip()
+    else
+        task.wait(0.16741)
+    end
 end
