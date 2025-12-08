26adf2cb6e0eb7cf3b87ad9ef6448d3129dc7ead
ryx67 <rtho49401@gmail.com>
2025-12-08 15:01:00 +0700

    Refactor game script for improved readability and functionality, including updates to NPC handling and UI elements

--- games/76558904092080.lua
+++ games/76558904092080.lua
@@ -5,8 +5,8 @@ local tweenService = cloneref(game:GetService('TweenService'))
 local debrisService = cloneref(game:GetService('Debris'))
 
 getgenv().SecureMode = true
-local Library = loadstring(game:HttpGet("https://raw.nebulasoftworks.xyz/starlight"))()  
-local Icons = loadstring(game:HttpGet("https://raw.nebulasoftworks.xyz/nebula-icon-library-loader"))()
+local Library = loadstring(game:HttpGet('https://raw.nebulasoftworks.xyz/starlight'))()  
+local Icons = loadstring(game:HttpGet('https://raw.nebulasoftworks.xyz/nebula-icon-library-loader'))()
 
 local lplr = playersService.LocalPlayer
 
@@ -16,10 +16,15 @@ local vars = {
 }
 
 local npcs = {}
-for i, v in pairs(workspace.Proximity:GetChildren()) do
+for i, v in ipairs(workspace.Proximity:GetChildren()) do
     if v:FindFirstChildOfClass('Humanoid') then
         table.insert(npcs, v)
     end
+end
+
+local npcnames = {}
+for i, v in ipairs(npcs) do
+    table.insert(npcnames, v.Name)
 end
 
 local function tweenTo(Position)
@@ -51,7 +56,7 @@ local function getRocks(Name)
 		for _, v in pairs(i:GetChildren()) do
 			if v:IsA('Part') and v:FindFirstChildOfClass('Model') then
 				local Target = v:FindFirstChildOfClass('Model')
-				if Target.Name == Name and Target:GetAttribute('Health') > 0 then
+				if Target.Name == Name and Target:FindFirstChild('Hitbox') and Target:GetAttribute('Health') > 0 then
 					return Target.Hitbox
 				end
 			end
@@ -60,75 +65,129 @@ local function getRocks(Name)
 end
 
 local Window = Library:CreateWindow({
-    Name = "ryx.wtf - The Forge",
-    Subtitle = "v1.0",
+    Name = 'ryx.wtf - The Forge',
+    Subtitle = 'v1.0',
     Icon = 124905402582719,
     InterfaceAdvertisingPrompts = false,
 
     LoadingSettings = {
-        Title = "My Script Hub",
-        Subtitle = "Welcome to My Script Hub",
+        Title = 'My Script Hub',
+        Subtitle = 'Welcome to My Script Hub',
     },
 
     FileSettings = {
-        ConfigFolder = "MyScript"
+        ConfigFolder = 'MyScript'
     },
 })
 
 Window:CreateHomeTab({
     SupportedExecutors = {'Ronix', 'Delta', 'Codex', 'Xeno', 'Solara'}, 
     UnsupportedExecutors = {},
-    DiscordInvite = "1234",
+    DiscordInvite = '1234',
     Backdrop = nil,
     IconStyle = 1, 
     Changelog = {
         {
-            Title = "Example Update",
-            Date = "25th october twentyfive",
-            Description = "blablblablajana \n blabakjakd",
+            Title = 'Example Update',
+            Date = '25th october twentyfive',
+            Description = 'blablblablajana \n blabakjakd',
         }
     }
 })
 
 local sections = {
-    farm = Window:CreateTabSection("Farming"),
-    settings = Window:CreateTabSection("Settings")
+    farm = Window:CreateTabSection('Farming'),
+    world = Window:CreateTabSection('Worlds'),
+    settings = Window:CreateTabSection('Settings')
 }
 
 local tabs = {
     mine = sections.farm:CreateTab({
-        Name = "Mine",
+        Name = 'Mine',
         Icon = Icons:GetIcon('pickaxe', 'Lucide'),
         Columns = 2,
-    }, "INDEX"),
+    }, 'INDEX'),
+
+    teleport = sections.world:CreateTab({
+        Name = 'Worlds',
+        Icon = Icons:GetIcon('map', 'Lucide'),
+        Columns = 2,
+    }, 'INDEX'),
 
     theme = sections.settings:CreateTab({
-        Name = "Theme",
+        Name = 'Theme',
         Icon = Icons:GetIcon('palette', 'Lucide'),
         Columns = 2,
-    }, "INDEX"),
+    }, 'INDEX'),
 
     config = sections.settings:CreateTab({
-        Name = "Config",
+        Name = 'Config',
         Icon = Icons:GetIcon('package-check', 'Lucide'),
         Columns = 2,
-    }, "INDEX"),
+    }, 'INDEX'),
 }
 
 local groups = {
     mining = tabs.mine:CreateGroupbox({
-        Name = "Mining",
+        Name = 'Mining',
         Column = 1,
-    }, "INDEX")
+    }, 'INDEX'),
+
+    mobs = tabs.mine:CreateGroupbox({
+        Name = 'Mobs',
+        Column = 1,
+    }, 'INDEX'),
+
+    npc = tabs.teleport:CreateGroupbox({
+        Name = 'Npc',
+        Column = 1,
+    }, 'INDEX'),
+
+    forge = tabs.teleport:CreateGroupbox({
+        Name = 'Forge',
+        Column = 2,
+    }, 'INDEX')
 }
 
 local automine = groups.mining:CreateToggle({
-    Name = "Auto attack ores",
+    Name = 'Auto attack ores',
     CurrentValue = false,
     Style = 2,
     Callback = function(Value)
         vars.automine = Value
     end,
+}, 'INDEX')
+
+for i, v in ipairs(npcnames) do
+    groups.npc:CreateButton({
+        Name = v,
+        Icon = Icons:GetIcon('circle-arrow-right', 'Lucide'),
+        Style = 1,
+        Callback = function()
+            local Entities = nil
+            for _, k in ipairs(npcs) do
+                if k.Name == v then
+                    Entities = k
+                    break
+                end
+            end
+            if Entities then
+                tweenTo(Entities.HumanoidRootPart.Position)
+            end
+        end,
+    }, "INDEX")
+end
+
+groups.forge:CreateButton({
+        Name = 'Forge',
+        Icon = Icons:GetIcon('circle-arrow-right', 'Lucide'),
+        Style = 1,
+        Callback = function()
+            local Forge = workspace.Forges:FindFirstChildOfClass('Model')
+            if Forge then
+                tweenTo(Forge.CFrames.CrucibleMelt.Position)
+            end
+        end,
 }, "INDEX")
 
 tabs.theme:BuildThemeGroupbox(1)
