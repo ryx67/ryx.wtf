a300143d830faaed19b944e2f13fa362e247706a
ryx67 <rtho49401@gmail.com>
2025-12-08 09:48:45 +0700

    Fix game loading mechanism by adding missing file extension and correcting error handling

--- loader.lua
+++ loader.lua
@@ -1,8 +1,9 @@
 local Repo = 'https://raw.githubusercontent.com/ryx67/ryx.wtf/refs/heads/main/'
 
 local suc, res = pcall(function()
-    return loadstring(game:HttpGet(Repo .. 'games/' .. game.PlaceId))()
-    if not suc or res == '404: Not Found' then
-        print('[ryx] -> game not supported')
-    end
+    return loadstring(game:HttpGet(Repo .. 'games/' .. game.PlaceId .. '.lua'))()
 end)
+
+if not suc or res == '404: Not Found' then
+    print('[ryx] -> game not supported')
+end
